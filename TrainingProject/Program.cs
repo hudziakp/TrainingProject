11a9b5d37f1ff77aff28307b05e9983c20e5c6c1
Piotr hudziakp <hudziakp@gmail.com>
2019-03-11 09:24:10 +0100

    Initial Commit

---
+++ TrainingProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace TrainingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// Prints Column name and values into the Console
+        /// </summary>
+        /// <param name="columnName">Column name</param>
+        /// <param name="values">String values for column</param>
+        internal static void PrintColumnValues(string columnName, string[] values)
+        {
+            if (values == null) throw new ArgumentNullException(nameof(values));
+            Console.Write($"{columnName}:");
+            var lastElement = values.Last();
+            foreach (var value in values)
+            {
+                Console.Write($"{value}");
+                if (!value.Equals(lastElement)) Console.Write(",");
+            }
+            Console.WriteLine("\n-----------------");
+        }
+    }
+}
---
+++ TrainingProject/ProgramTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace TrainingProject
+{
+    [TestFixture]
+    class ProgramTest
+    {
+        [Test]
+        public void TestPrintColumnValues()
+        {
+            using (var stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+                Program.PrintColumnValues("Names", new[] { "Jack", "Greg", "Richard" });
+                var output = stringWriter.ToString();
+                StringAssert.StartsWith("Names:", output);
+                StringAssert.Contains("Jack", output);
+                StringAssert.Contains("Greg", output);
+                StringAssert.Contains("Richard", output);
+            }
+        }
+    }
+}
