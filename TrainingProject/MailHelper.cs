4b09e1fc9a057d417c715bf4877b4226457bc808
Bartłomiej Kołodziejczyk <kolodz.bart@gmail.com>
2019-04-10 23:24:15 +0200

    Fixing with libraries

--- TrainingProject/MailHelper.cs
+++ TrainingProject/MailHelper.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace TrainingProject
 {
